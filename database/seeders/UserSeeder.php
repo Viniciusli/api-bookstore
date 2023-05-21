4baa41830f574c9a1c9c6ae156a78c2238823eed
Vinicius Lee <Viniciusleeg@outlook.com>
2023-05-21 13:23:53 -0300

    preparando os testes

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'vinilee34@gmail.com',
+            'password' => '12345678',
+        ]);
+
+        $user->assignRole('Super Admin');
+    }
+}
